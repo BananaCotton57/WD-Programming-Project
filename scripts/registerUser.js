dfb4bd9ea60d2410d9e2fdab77ce9e597b6e073a
BananaCotton57 <johnnyngo772@gmail.com>
2025-11-04 19:18:01 -0500

    createed functionality for the register page

--- scripts/registerUser.js
+++ scripts/registerUser.js
@@ -8,5 +8,24 @@ console.log(registerForm)
 function register(e){
     e.preventDefault()
 
+    password = document.getElementById("password").value
+    confirmPassword = document.getElementById("password_confirm").value
+
+    if(validPassword(password, confirmPassword)){
+    const user = {
+        username: document.getElementById("username").value,
+        email: document.getElementById("email").value,
+        password: document.getElementById("password").value
+    }
+
     console.log("Register Function Ran!!!")
+    console.log(user);
+    
+    } else {
+        console.log("Your passwords don't match!!!!!!!")
+    }
+}
+
+function validPassword(password, confirmPassword){
+    if(password === confirmPassword) return true
 }
